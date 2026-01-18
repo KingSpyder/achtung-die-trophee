c22eac49f84771d67beeeff92232bf22825a4fa1
Nicolas Gandon <nicolas.gandon78@gmail.com>
2026-01-18 17:21:43 +0100

    Lobby GUI visuals

--- achtung-remake-godot/src/home/main_menu.gd
+++ achtung-remake-godot/src/home/main_menu.gd
@@ -1,14 +1,8 @@
 extends Control
 
 func _ready() -> void:
-	var host_button = $Host
-	var join_button = $Join
-	var start_online_button = $"Start Online"
+	pass
 	
-	host_button.button_down.connect(NetworkController._on_host_button_down)
-	join_button.button_down.connect(NetworkController._on_join_button_down)
-	start_online_button.button_down.connect(NetworkController._on_start_online_button_down)
-
 func _on_add_player_button_down() -> void:
 	var playerSize = GameManager.players.size()
 	var id = playerSize + 2
